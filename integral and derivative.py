334897811c12f30a6d80201dc7edf0279b7db96f
sei410 <cald410@gmail.com>
2021-03-08 16:57:16 +0900

    main追加

--- integral and derivative.py
+++ integral and derivative.py
@@ -9,19 +9,28 @@ I = sym.S.ImaginaryUnit # 虚数単位
 oo = sym.oo # 無限大
 (a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z) = sym.symbols('a b c d e f g h i j k l m n o p q r s t u v w x y z')
 
-def limit():
-    print("y = ")
-    expr = input()
-    Y = sym.Eq(expr)
-    print(sym.limit(2 ** (3 * x - 1) / (Y ** 2), x, oo))
+def limit(Y):
+    tmp = input("n → ")
+    tmp = sym.Eq(tmp,0)
+    return sym.limit(Y, x, tmp)
 
-def integral():
-    print("y = ")
-    expr = input()
-    Y = sym.Eq(expr)
+def derivative(Y):
+    return sym.diff(Y)
 
-def derivative():
-    print("y = ")
-    expr = input()
-    Y = sym.Eq(expr)
+def integral(Y):
+    return sym.integrate(Y.x)
 
+expr = input("y = ")
+Y = sym.Eq(expr, 0)
+print("1:極限計算")
+print("2:微分")
+print("3:積分")
+num=int(input("入力: "))
+
+if num==1:
+    ans=limit(Y)
+elif num==2:
+    ans = derivative(Y)
+else:
+    ans=integral(Y)
+print(ans)
