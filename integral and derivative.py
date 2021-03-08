fb77b9f7fcabb617bed001b31d0a6016fc511c64
sei410 <cald410@gmail.com>
2021-03-08 16:32:11 +0900

    極限,微分,積分の3関数を用意

--- integral and derivative.py
+++ integral and derivative.py
@@ -1,5 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-import sympy
+import sympy as sym
+sym.init_printing()
+Pi = sym.S.Pi # 円周率
+E = sym.S.Exp1 # 自然対数の底
+I = sym.S.ImaginaryUnit # 虚数単位
+oo = sym.oo # 無限大
+(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z) = sym.symbols('a b c d e f g h i j k l m n o p q r s t u v w x y z')
+
+def limit():
+    print("y = ")
+    expr = input()
+    Y = sym.Eq(expr)
+    print(sym.limit(2 ** (3 * x - 1) / (Y ** 2), x, oo))
+
+def integral():
+    print("y = ")
+    expr = input()
+    Y = sym.Eq(expr)
+
+def derivative():
+    print("y = ")
+    expr = input()
+    Y = sym.Eq(expr)
 
